1e5c58452452f60bfa29236af0352e7ef4d2e004
Honza Javorek <jan.javorek@gmail.com>
2012-08-04 15:10:11 +0200

    kino script and requirements management

--- README.md
+++ README.md
@@ -28,3 +28,7 @@ The first repo is the one with script (`pubfile.py`). The other one has to have 
 If you are done with this, commit your changes. Then create your Heroku app by `heroku create` and `git push heroku master`. Be sure you have no all-time-running processes: `heroku ps:scale worker=0`. Add scheduler by `heroku addons:add scheduler:standard` and adjust it's settings on admin panel. Use `worker` as the task name.
 
 You can test & debug by `foreman start` (locally) or `heroku run python pub.py` (remotely, Heroku stack).
+
+## Requirements
+
+Pub has no requirements.
--- pub.py
+++ pub.py
@@ -27,43 +27,43 @@ config = {
 
 class Publisher(object):
 
-    def _set_author(self):
+    def set_author(self):
         call(['git', 'config', 'user.name', config['name']], cwd=config['pages_dir'])
         call(['git', 'config', 'user.email', config['email']], cwd=config['pages_dir'])
 
-    def _commit(self, message):
+    def commit(self, message):
         message = message or 'automatic update'
         call(['git', 'add', '-A'], cwd=config['pages_dir'])
         call(['git', 'commit', '-m', message], cwd=config['pages_dir'])
 
-    def _push(self):
+    def push(self):
         call(['git', 'push', 'origin', 'gh-pages'], cwd=config['pages_dir'])
         call(['rm', '-rf', config['tmp_dir']])
 
     def run(self, message=None):
-        self._set_author()
-        self._commit(message)
-        self._push()
+        self.set_author()
+        self.commit(message)
+        self.push()
 
 
 class Downloader(object):
 
-    def _compose_url(self, repo):
+    def compose_url(self, repo):
         return 'https://{username}:{password}@github.com/{repo}.git'.format(
             username=config['username'],
             password=config['password'],
             repo=repo
         )
 
-    def _compose_command(self, repo, dir, branch=None):
-        url = self._compose_url(repo)
+    def compose_command(self, repo, dir, branch=None):
+        url = self.compose_url(repo)
         parts = ['git', 'clone']
         if branch:
             parts += ['-b', branch]
         return parts + [url, dir]
 
     def run(self, repo, dir, branch=None):
-        call(self._compose_command(repo, dir, branch))
+        call(self.compose_command(repo, dir, branch))
 
 
 class Task(object):
@@ -75,19 +75,22 @@ class Task(object):
     def log(self, message):
         sys.stderr.write('[pub] {}\n'.format(message))
 
-    def _prepare_tmp_dir(self):
+    def prepare_tmp_dir(self):
         call(['rm', '-rf', config['tmp_dir']])
         call(['mkdir', config['tmp_dir']])
 
-    def _read_definitions(self):
+    def read_definitions(self):
         with open(config['scripts_file']) as f:
             lines = f.readlines()
         for line in lines:
             yield re.split(r'\s+', line)
 
-    def _process(self, script_repo, pages_repo):
+    def install(self, requirements):
+        call(['pip', 'install', '-r', requirements])
+
+    def process(self, script_repo, pages_repo):
         # prepare tmp directory
-        self._prepare_tmp_dir()
+        self.prepare_tmp_dir()
 
         # download both script and pages
         self.log('Downloading script.')
@@ -102,19 +105,29 @@ class Task(object):
             branch='gh-pages'
         )
 
-        # run script's pubfile.py
-        self.log('Calling pubfile.py.')
-        call(['python', 'pubfile.py'], cwd=config['tmp_dir'])
+        # install requirements
+        requirements = os.path.join(config['tmp_dir'], 'requirements.txt')
+        if os.path.exists(requirements):
+            self.log('Installing requirements.')
+            self.install(requirements)
 
-        # publish changes
-        self.log('Publishing.')
-        self.publisher.run()
+        try:
+            # run script's pubfile.py
+            self.log('Calling pubfile.py.')
+            call(['python', 'pubfile.py'], cwd=config['tmp_dir'])
+
+            # publish changes
+            self.log('Publishing.')
+            self.publisher.run()
+
+        except ImportError:
+            self.log('Missing requirements.')
 
     def run(self):
         self.log('Starting.')
-        for script_repo, pages_repo in self._read_definitions():
+        for script_repo, pages_repo in self.read_definitions():
             self.log('Script {} to pages {}.'.format(script_repo, pages_repo))
-            self._process(script_repo, pages_repo)
+            self.process(script_repo, pages_repo)
         self.log('Terminating.')
 
 
